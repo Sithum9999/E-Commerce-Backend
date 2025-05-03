d7dba0010c092aae61921757c5068e79355be9ea
Sithum9999 <sankalpasithum51@gmail.com>
2025-05-03 10:11:19 +0530

    created Main.java class

--- src/main/java/org/icet/learn/Main.java
+++ src/main/java/org/icet/learn/Main.java
@@ -1,7 +1,11 @@
 package org.icet.learn;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-
+        SpringApplication.run(Main.class);
     }
 }
