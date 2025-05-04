712f9a32f1ea571fc8e5d30eaced385aceef86aa
Sithum9999 <sankalpasithum51@gmail.com>
2025-05-04 13:40:41 +0530

    created UserDao.java interface

---
+++ src/main/java/org/icet/learn/repository/UserDao.java
@@ -0,0 +1,13 @@
+package org.icet.learn.repository;
+
+import org.icet.learn.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserDao extends JpaRepository<UserEntity,Long> {
+
+    Optional<UserEntity> findFirstByEmail(String username);
+}
