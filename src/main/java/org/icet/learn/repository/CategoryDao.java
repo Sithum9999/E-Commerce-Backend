386cd0bb46c5b35593cdf0390f1cd55f77f303f3
Sithum9999 <sankalpasithum51@gmail.com>
2025-05-09 16:24:04 +0530

    create CategoryDao interface

---
+++ src/main/java/org/icet/learn/repository/CategoryDao.java
@@ -0,0 +1,9 @@
+package org.icet.learn.repository;
+
+import org.icet.learn.entity.CategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryDao extends JpaRepository<CategoryEntity, Long> {
+}
