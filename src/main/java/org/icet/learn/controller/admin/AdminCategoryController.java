f919358d7edbcb403580a661b090abae6aa0b53e
Sithum9999 <sankalpasithum51@gmail.com>
2025-05-09 16:25:05 +0530

    create add category end point in AdminCategoryController class

---
+++ src/main/java/org/icet/learn/controller/admin/AdminCategoryController.java
@@ -0,0 +1,26 @@
+package org.icet.learn.controller.admin;
+
+import lombok.RequiredArgsConstructor;
+import org.icet.learn.dto.Category;
+import org.icet.learn.entity.CategoryEntity;
+import org.icet.learn.service.admin.category.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
+public class AdminCategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping("category")
+    public ResponseEntity<CategoryEntity> createCategory(@RequestBody Category category){
+        CategoryEntity categoryEntity = categoryService.createCategory(category);
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoryEntity);
+    }
+}
