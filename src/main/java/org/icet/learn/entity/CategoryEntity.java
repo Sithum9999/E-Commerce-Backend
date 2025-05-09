12bb70912ae42faebaa93844c3a3325315f60a13
Sithum9999 <sankalpasithum51@gmail.com>
2025-05-09 16:22:08 +0530

    create CategoryEntity class and ProductEntity class

---
+++ src/main/java/org/icet/learn/entity/CategoryEntity.java
@@ -0,0 +1,22 @@
+package org.icet.learn.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Table(name = "category")
+@Data
+@AllArgsConstructor
+public class CategoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Lob
+    private String description;
+
+}
---
+++ src/main/java/org/icet/learn/entity/ProductEntity.java
@@ -0,0 +1,35 @@
+package org.icet.learn.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Data
+@Table(name = "product")
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Long price;
+
+    @Lob
+    private String description;
+
+    @Lob
+    @Column(columnDefinition = "longblob")
+    private byte[] img;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "category_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private CategoryEntity categoryEntity;
+
+}
