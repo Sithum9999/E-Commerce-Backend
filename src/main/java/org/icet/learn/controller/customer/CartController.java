d659493ce9ef7dda75806084e1ee044610d2d674
Sithum9999 <sankalpasithum51@gmail.com>
2025-05-18 18:49:21 +0530

    created getCartByUserId end point

--- src/main/java/org/icet/learn/controller/customer/CartController.java
+++ src/main/java/org/icet/learn/controller/customer/CartController.java
@@ -3,7 +3,9 @@ package org.icet.learn.controller.customer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.icet.learn.dto.AddProductInCart;
+import org.icet.learn.dto.Order;
 import org.icet.learn.service.customer.cart.CartService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +21,12 @@ public class CartController {
     public ResponseEntity<?> addProductToCart(@RequestBody AddProductInCart addProductInCart) {
         log.info("Cart: "+addProductInCart);
         return cartService.addProductToCart(addProductInCart);
+    }
+
+    @GetMapping("/cart/{userId}")
+    public ResponseEntity<?> getCartByUserId(@PathVariable Long userId){
+        Order orderDto = cartService.getCartByUserId(userId);
+        return ResponseEntity.status(HttpStatus.OK).body(orderDto);
     }
 
 }
