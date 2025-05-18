9d567affa99ca36a203974efb41d0288cfa368a3
Sithum9999 <sankalpasithum51@gmail.com>
2025-05-18 17:45:58 +0530

    update errors and not directly return the CartItemsEntity map the CartItemsEntity to CartItem dto because Jackson can't serialize this directly

--- src/main/java/org/icet/learn/service/customer/cart/CartServiceImpl.java
+++ src/main/java/org/icet/learn/service/customer/cart/CartServiceImpl.java
@@ -1,7 +1,9 @@
 package org.icet.learn.service.customer.cart;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.icet.learn.dto.AddProductInCart;
+import org.icet.learn.dto.CartItem;
 import org.icet.learn.entity.CartItemsEntity;
 import org.icet.learn.entity.OrderEntity;
 import org.icet.learn.entity.ProductEntity;
@@ -17,6 +19,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CartServiceImpl implements CartService{
@@ -30,23 +33,27 @@ public class CartServiceImpl implements CartService{
     private final ProductDao productDao;
 
     public ResponseEntity<?> addProductToCart(AddProductInCart addProductInCart) {
-    OrderEntity activeOrder = orderDao.findByUserEntityIdAndOrderStatus(addProductInCart.getUserId(), OrderStatus.Pending);
-        Optional<CartItemsEntity> optionalCartItems = cartItemDao.findByProductEntityIdAndOrderEntityIdAndUserEntityId(
+        OrderEntity activeOrder = orderDao.findByUserIdAndOrderStatus(
+                addProductInCart.getUserId(), OrderStatus.Pending);
+
+        Optional<CartItemsEntity> optionalCartItems = cartItemDao.findByProductIdAndOrderIdAndUserId(
                 addProductInCart.getProductId(), activeOrder.getId(), addProductInCart.getUserId());
 
         if (optionalCartItems.isPresent()) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body(null);
         } else {
-            Optional<ProductEntity> optionalProduct = productDao.findById(addProductInCart.getProductId());
-            Optional<UserEntity> optionalUser = userDao.findById(addProductInCart.getUserId());
+            Optional<ProductEntity> optionalProduct = productDao.findById(
+                    addProductInCart.getProductId());
+            Optional<UserEntity> optionalUser = userDao.findById(
+                    addProductInCart.getUserId());
 
             if (optionalProduct.isPresent() && optionalUser.isPresent()) {
                 CartItemsEntity cart = new CartItemsEntity();
-                cart.setProductEntity(optionalProduct.get());
+                cart.setProduct(optionalProduct.get());
                 cart.setPrice(optionalProduct.get().getPrice());
                 cart.setQuantity(1L);
-                cart.setUserEntity(optionalUser.get());
-                cart.setOrderEntity(activeOrder);
+                cart.setUser(optionalUser.get());
+                cart.setOrder(activeOrder);
 
                 CartItemsEntity updatedCart = cartItemDao.save(cart);
 
@@ -56,11 +63,27 @@ public class CartServiceImpl implements CartService{
 
                 orderDao.save(activeOrder);
 
-                return ResponseEntity.status(HttpStatus.CREATED).body(cart);
-            } else {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User or product not found");
-            }
+                CartItem cartDto = mapToDto(updatedCart);
+                return ResponseEntity.status(HttpStatus.CREATED).body(cartDto);
+
+
+            }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User or product not found");
         }
+
     }
+    }
+
+    private CartItem mapToDto(CartItemsEntity entity) {
+        CartItem dto = new CartItem();
+        dto.setId(entity.getId());
+        dto.setPrice(entity.getPrice());
+        dto.setQuantity(entity.getQuantity());
+        dto.setProductId(entity.getProduct().getId());
+        dto.setProductName(entity.getProduct().getName());
+        return dto;
+    }
+
+
 
 }
