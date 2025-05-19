753ef99389cb72d6ac3c6240f63b3c0692f6acf9
Sithum9999 <sankalpasithum51@gmail.com>
2025-05-20 00:50:30 +0530

    throw validation class

---
+++ src/main/java/org/icet/learn/exceptions/ValidationException.java
@@ -0,0 +1,9 @@
+package org.icet.learn.exceptions;
+
+public class ValidationException extends RuntimeException{
+
+    public ValidationException (String message){
+        super(message);
+    }
+
+}
