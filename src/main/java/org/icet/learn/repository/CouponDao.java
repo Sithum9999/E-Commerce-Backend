22184fbc16f026ca874dcf6fd99baca501d480d0
Sithum9999 <sankalpasithum51@gmail.com>
2025-05-22 14:05:25 +0530

    create findByCode()

--- src/main/java/org/icet/learn/repository/CouponDao.java
+++ src/main/java/org/icet/learn/repository/CouponDao.java
@@ -4,7 +4,10 @@ import org.icet.learn.entity.CouponEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CouponDao extends JpaRepository<CouponEntity, Long> {
     boolean existsByCode(String code);
+    Optional<CouponEntity> findByCode(String code);
 }
