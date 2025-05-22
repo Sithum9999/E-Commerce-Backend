01945e50ef08d0abda46d2689e6c7d6a91b86749
Sithum9999 <sankalpasithum51@gmail.com>
2025-05-22 14:06:14 +0530

    updated OrderEntity.java class

--- src/main/java/org/icet/learn/entity/OrderEntity.java
+++ src/main/java/org/icet/learn/entity/OrderEntity.java
@@ -2,6 +2,7 @@ package org.icet.learn.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.icet.learn.dto.Order;
 import org.icet.learn.enums.OrderStatus;
 
 import java.util.Date;
@@ -38,7 +39,31 @@ public class OrderEntity {
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private UserEntity user;
 
+    @OneToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "coupon_id", referencedColumnName = "id")
+    private CouponEntity coupon;
+
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "order")
     private List<CartItemsEntity> cartItems;
+
+    public Order getOrderDto() {
+        Order order = new Order();
+
+        order.setId(id); //
+        order.setOrderDescription(orderDescription);
+        order.setAddress(address);
+        order.setTrackingId(trackingId);
+        order.setAmount(amount);
+        order.setDate(date);
+        order.setOrderStatus(orderStatus);
+        order.setUsername(user.getName());
+
+        if (coupon != null) {
+            order.setCouponName(coupon.getName());
+        }
+
+        return order;
+    }
+
 }
 
