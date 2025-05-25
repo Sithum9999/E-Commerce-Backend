b42adbb38d32a97290a20d33eb9f05fd61028378
Sithum9999 <sankalpasithum51@gmail.com>
2025-05-26 00:53:48 +0530

    create changeOrderStatus() end point in AdminOrderController.java class

--- src/main/java/org/icet/learn/controller/admin/AdminOrderController.java
+++ src/main/java/org/icet/learn/controller/admin/AdminOrderController.java
@@ -3,8 +3,10 @@ package org.icet.learn.controller.admin;
 import lombok.RequiredArgsConstructor;
 import org.icet.learn.dto.Order;
 import org.icet.learn.service.admin.order.AdminOrderService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,15 @@ public class AdminOrderController {
     @GetMapping("/placedOrders")
     public ResponseEntity<List<Order>> getAllPlacedOrders() {
         return ResponseEntity.ok(adminOrderService.getAllPlacedOrders());
+    }
+
+    @GetMapping("/order/{orderId}/{status}")
+    public ResponseEntity<?> changeOrderStatus(@PathVariable Long orderId, @PathVariable String status) {
+        Order order = adminOrderService.changeOrderStatus(orderId, status);
+        if (order == null) {
+            return new ResponseEntity<>("Something went wrong!", HttpStatus.BAD_REQUEST);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(order);
     }
 
 }
