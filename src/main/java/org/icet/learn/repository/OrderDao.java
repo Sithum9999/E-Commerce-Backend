cc2f11d4378f23337bdc3b9fa92ef760e330b361
Sithum9999 <sankalpasithum51@gmail.com>
2025-05-25 18:01:52 +0530

    create findAllByOrderStatusIn() in OrderDao.java interface

--- src/main/java/org/icet/learn/repository/OrderDao.java
+++ src/main/java/org/icet/learn/repository/OrderDao.java
@@ -5,7 +5,10 @@ import org.icet.learn.enums.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderDao extends JpaRepository<OrderEntity, Long> {
     OrderEntity findByUserIdAndOrderStatus(Long userId, OrderStatus orderStatus);
+    List<OrderEntity> findAllByOrderStatusIn(List<OrderStatus> orderStatusList);
 }
