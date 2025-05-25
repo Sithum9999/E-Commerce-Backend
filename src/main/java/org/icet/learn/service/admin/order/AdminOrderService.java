843906efed501dc3e778a5ea5901ab6f5428b1b0
Sithum9999 <sankalpasithum51@gmail.com>
2025-05-25 18:01:15 +0530

    put getAllPlacedOrders() signature to AdminOrderService.java interface

---
+++ src/main/java/org/icet/learn/service/admin/order/AdminOrderService.java
@@ -0,0 +1,9 @@
+package org.icet.learn.service.admin.order;
+
+import org.icet.learn.dto.Order;
+
+import java.util.List;
+
+public interface AdminOrderService {
+    List<Order> getAllPlacedOrders();
+}
