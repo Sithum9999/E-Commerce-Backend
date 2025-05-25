1021cf6fafe14b640f37e0013438a14640c746e5
Sithum9999 <sankalpasithum51@gmail.com>
2025-05-26 00:48:30 +0530

    create changeOrderStatus() in AdminOrderServiceImpl.java class and put this method signature in AdminOrderService interface

--- src/main/java/org/icet/learn/service/admin/order/AdminOrderService.java
+++ src/main/java/org/icet/learn/service/admin/order/AdminOrderService.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public interface AdminOrderService {
     List<Order> getAllPlacedOrders();
+    Order changeOrderStatus(Long orderId, String status);
 }
--- src/main/java/org/icet/learn/service/admin/order/AdminOrderServiceImpl.java
+++ src/main/java/org/icet/learn/service/admin/order/AdminOrderServiceImpl.java
@@ -8,6 +8,8 @@ import org.icet.learn.repository.OrderDao;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,6 +21,21 @@ public class AdminOrderServiceImpl implements AdminOrderService{
     public List<Order> getAllPlacedOrders() {
         List<OrderEntity> orderList = orderDao.findAllByOrderStatusIn(List.of(OrderStatus.Placed, OrderStatus.Shipped, OrderStatus.Delivered));
         return orderList.stream().map(OrderEntity::getOrderDto).collect(Collectors.toList());
+    }
+
+    public Order changeOrderStatus(Long orderId, String status) {
+        Optional<OrderEntity> optionalOrder = orderDao.findById(orderId);
+        if (optionalOrder.isPresent()) {
+            OrderEntity order = optionalOrder.get();
+
+            if (Objects.equals(status, "Shipped")) {
+                order.setOrderStatus(OrderStatus.Shipped);
+            } else if (Objects.equals(status, "Delivered")) {
+                order.setOrderStatus(OrderStatus.Delivered);
+            }
+            return orderDao.save(order).getOrderDto();
+        }
+        return null;
     }
 
 }
