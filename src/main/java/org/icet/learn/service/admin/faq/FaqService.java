ab7871de4297d672c5254854144005b630485f50
Sithum9999 <sankalpasithum51@gmail.com>
2025-05-26 13:10:49 +0530

    create FaqService.java and FaqServiceImpl.java

---
+++ src/main/java/org/icet/learn/service/admin/faq/FaqService.java
@@ -0,0 +1,4 @@
+package org.icet.learn.service.admin.faq;
+
+public interface FaqService {
+}
---
+++ src/main/java/org/icet/learn/service/admin/faq/FaqServiceImpl.java
@@ -0,0 +1,10 @@
+package org.icet.learn.service.admin.faq;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FaqServiceImpl implements FaqService{
+
+}
