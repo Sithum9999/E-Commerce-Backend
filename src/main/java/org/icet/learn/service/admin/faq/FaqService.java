72f8cef2d617e0eddfa90d50d77ac994b40458a1
Sithum9999 <sankalpasithum51@gmail.com>
2025-05-26 14:43:32 +0530

    create postFAQ() in FaqServiceImpl.java and put method signature on FaqService interface

--- src/main/java/org/icet/learn/service/admin/faq/FaqService.java
+++ src/main/java/org/icet/learn/service/admin/faq/FaqService.java
@@ -1,4 +1,9 @@
 package org.icet.learn.service.admin.faq;
 
+import org.icet.learn.dto.Faq;
+
 public interface FaqService {
+
+    Faq postFAQ(Long productId, Faq faqDto);
+
 }
--- src/main/java/org/icet/learn/service/admin/faq/FaqServiceImpl.java
+++ src/main/java/org/icet/learn/service/admin/faq/FaqServiceImpl.java
@@ -1,10 +1,33 @@
 package org.icet.learn.service.admin.faq;
 
 import lombok.RequiredArgsConstructor;
+import org.icet.learn.dto.Faq;
+import org.icet.learn.entity.FaqEntity;
+import org.icet.learn.entity.ProductEntity;
+import org.icet.learn.repository.FaqDao;
+import org.icet.learn.repository.ProductDao;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class FaqServiceImpl implements FaqService{
+
+    private final FaqDao faqDao;
+    private final ProductDao productDao;
+
+    public Faq postFAQ(Long productId, Faq faqDto) {
+        Optional<ProductEntity> optionalProduct = productDao.findById(productId);
+        if (optionalProduct.isPresent()) {
+            FaqEntity faq = new FaqEntity();
+
+            faq.setQuestion(faqDto.getQuestion());
+            faq.setAnswer(faqDto.getAnswer());
+            faq.setProduct(optionalProduct.get());
+            return faqDao.save(faq).getFaqDto();
+        }
+        return null;
+    }
 
 }
