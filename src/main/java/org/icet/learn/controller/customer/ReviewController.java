beca6d93dfbfc654615b7c41dfce67796ac28af2
Sithum9999 <sankalpasithum51@gmail.com>
2025-05-29 15:10:41 +0530

    fix error and update this

--- src/main/java/org/icet/learn/controller/customer/ReviewController.java
+++ src/main/java/org/icet/learn/controller/customer/ReviewController.java
@@ -30,7 +30,7 @@ public class ReviewController {
         if (reviewDto1 == null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Something went wrong");
         }
-        return ResponseEntity.status(HttpStatus.CREATED).body(reviewDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(reviewDto1);
     }
 
 }
